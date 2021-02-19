24d863b74f2a500963b7dee57ae267d80d13a385
HuiUofT <huisci@gmail.com>
2021-02-19 07:32:45 -0500

    test

---
+++ R/hello.R
@@ -0,0 +1,13 @@
+#' This function is used to extract peak features
+#'
+#' @param x 
+#' @param y 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+peak_extract <- function(x,y) {
+  print(x+y)
+}
+#‘ devtools::document()
